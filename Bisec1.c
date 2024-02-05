5ca8c7c234d180fd1054e49ae7132781984bdca7
bishnulimbu <subashlimbu987654321@gmail.com>
2024-02-05 09:33:32 +0545

    dfdfd

--- Bisec1.c
+++ Bisec1.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include <math.h>
 
-float f(float x) { return pow(x, 3) + 2 * x - 1; };
+float f(float x) { return pow(x, 3) - 2 * x - 5; };
 
 int main() {
   float x1, x2, x0, x, err = 0.001;
@@ -12,13 +12,16 @@ int main() {
   if (f(x1) * f(x2) >= 0) {
     printf("\n invalid case");
   }
+  int i = 1;
   do {
     x0 = (x1 + x2) / 2;
+    printf("%d\t%f\t%f\t%f\n", i, x1, x2, x0);
     if (f(x1) * f(x0) > 0) {
       x1 = x0;
     } else {
       x2 = x0;
     }
+    i++;
   } while (fabs(x2 - x1) > err);
   printf("%f", x0);
 
