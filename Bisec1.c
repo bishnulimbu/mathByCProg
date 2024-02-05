199b0d71e08bf978c8f80bd5c52274c89a5ec931
bishnulimbu <subashlimbu987654321@gmail.com>
2024-02-05 09:23:33 +0545

    dfd

--- Bisec1.c
+++ Bisec1.c
@@ -5,7 +5,7 @@ float f(float x) { return pow(x, 3) + 2 * x - 1; };
 
 int main() {
   printf("hello world1");
-  float x1, x2, x0, x;
+  float x1, x2, x0, x, err = 0.001;
   printf("enter the first case.");
   scanf("%f", &x1);
   printf("enter the second case.");
@@ -13,6 +13,15 @@ int main() {
   if (f(x1) * f(x2) >= 0) {
     printf("\n invalid case");
   }
+  do {
+    x0 = (x1 + x2) / 2;
+    if (f(x1) * f(x0) > 0) {
+      x0 = x1;
+    } else {
+      x0 = x2;
+    }
+  } while (fabs(f(x0)) > err);
+  printf("%f", x0);
 
   return 0;
 }
