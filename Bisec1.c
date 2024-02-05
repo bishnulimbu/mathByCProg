dee53e92de47b722dc78450ba5d3f890a6d7363c
bishnulimbu <subashlimbu987654321@gmail.com>
2024-02-05 09:18:40 +0545

    dfd

--- Bisec1.c
+++ Bisec1.c
@@ -5,6 +5,10 @@ float f(float x) { return pow(x, 3) + 2 * x - 1; };
 
 int main() {
   printf("hello world1");
+  float x1, x2, x0, x;
+  if (f(x1) * f(x2) >= 0) {
+    printf("\n invalid case");
+  }
 
   return 0;
 }
